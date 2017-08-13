924bfab35af309be27cfa447e049b2007cedc81c
Bill <bill.nonbusiness@gmail.com>
2017-08-12 22:23:42 -0400

    Add files via upload

---
+++ Coordinates.java
@@ -0,0 +1,20 @@
+package tetris.game;
+
+public class Coordinates{
+
+    final private int x;
+    final private int y;
+
+    public Coordinates(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+}
